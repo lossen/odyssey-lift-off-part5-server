24de203a03710e6123923ad1cd1d00f0a392351b
Anna Logacheva <anna.s.litvinenko@gmail.com>
2023-01-03 20:47:17 +0600

    Deprecated length in track and module

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,6 +44,7 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    durationInSeconds: ({ length }) => length, // length from first parent parameter because of resolver chain
   },
 };
 
